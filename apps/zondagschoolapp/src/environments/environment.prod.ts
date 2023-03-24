9dd33392162780fd9bca7f9121d3c00387cd1b0a
TurboSjaantje <daanvdm@hotmail.com>
2023-03-24 17:38:30 +0100

    deploying neo4j api

--- apps/zondagschoolapp/src/environments/environment.prod.ts
+++ apps/zondagschoolapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://monorepo-production-f725.up.railway.app',
-  neoUrl: 'https://monorepo-production-9f54.up.railway.app',
+  neoUrl: 'https://monorepo-production-9f54.up.railway.app'
 };
